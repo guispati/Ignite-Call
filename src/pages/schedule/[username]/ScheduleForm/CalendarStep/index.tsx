7bfd0620e08b1174b893e223ad0c31a8b951d597
Guilherme Spati <guilherme.spati723@gmail.com>
2023-01-05 15:00:51 -0300

    agendamento concluido

--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -11,7 +11,11 @@ interface Availability {
 	availableTimes: number[];
 }
 
-export function CalendarStep() {
+interface CalendarStepProps {
+	onSelectDateTime: (data: Date) => void;
+}
+
+export function CalendarStep({ onSelectDateTime }: CalendarStepProps) {
 	const [selectedDate, setSelectedDate] = useState<Date | null>(null);
 	// const [availability, setAvailability] = useState<Availability | null>(null);
 
@@ -52,6 +56,12 @@ export function CalendarStep() {
 	// 	})
 	// }, [selectedDate, username]);
 
+	function handleSelectTime(hour: number) {
+		const dateWithTime = dayjs(selectedDate).set("hour", hour).startOf('hour').toDate();
+
+		onSelectDateTime(dateWithTime);
+	}
+
 	return (
 		<Container isTimePickerOpen={isDateSelected}>
 			<Calendar selectedDate={selectedDate} onSelectedDate={setSelectedDate} />
@@ -67,6 +77,7 @@ export function CalendarStep() {
 							return (
 								<TimePickerItem
 									key={hour}
+									onClick={() => handleSelectTime(hour)}
 									disabled={!availability.availableTimes.includes(hour)}
 								>
 									{String(hour).padStart(2, '0')}:00h
--- src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
@@ -1,8 +1,11 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Button, Text, TextArea, TextInput } from "@ignite-ui/react";
+import dayjs from "dayjs";
+import { useRouter } from "next/router";
 import { CalendarBlank, Clock } from "phosphor-react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
+import { api } from "../../../../../lib/axios";
 import { ConfirmForm, FormActions, FormError, FormHeader } from "./styles";
 
 const confirmFormSchema = z.object({
@@ -11,9 +14,14 @@ const confirmFormSchema = z.object({
 	observations: z.string().nullable(),
 });
 
-type ConfirmFormData = z.infer<typeof confirmFormSchema>
+type ConfirmFormData = z.infer<typeof confirmFormSchema>;
 
-export function ConfirmStep() {
+interface ConfirmStepProps {
+	schedulingDate: Date;
+	onCancelConfirmation: () => void;
+}
+
+export function ConfirmStep({ schedulingDate, onCancelConfirmation }: ConfirmStepProps) {
 	const {
 		register,
 		handleSubmit,
@@ -22,19 +30,35 @@ export function ConfirmStep() {
 		resolver: zodResolver(confirmFormSchema)
 	});
 
-	function handleConfirmScheduling(data: ConfirmFormData) {
+	const router = useRouter();
+	const username = String(router.query.username);
+
+	async function handleConfirmScheduling(data: ConfirmFormData) {
+		const { name, email, observations } = data;
+
+		await api.post(`/users/${username}/schedule`, {
+			name,
+			email,
+			observations,
+			date: schedulingDate,
+		});
+
+		onCancelConfirmation();
 	}
+
+	const describedDate = dayjs(schedulingDate).format('DD[ de ]MMMM[ de ]YYYY');
+	const describedTime = dayjs(schedulingDate).format('HH:mm[h]');
 
 	return (
 		<ConfirmForm as="form" onSubmit={handleSubmit(handleConfirmScheduling)}>
 			<FormHeader>
 				<Text>
 					<CalendarBlank />
-					22 de setembro de 2022
+					{describedDate}
 				</Text>
 				<Text>
 					<Clock />
-					18:00h
+					{describedTime}
 				</Text>
 			</FormHeader>
 
@@ -60,7 +84,7 @@ export function ConfirmStep() {
 			</label>
 
 			<FormActions>
-				<Button type="button" variant="tertiary">Cancelar</Button>
+				<Button type="button" variant="tertiary" onClick={onCancelConfirmation}>Cancelar</Button>
 				<Button type="submit" disabled={isSubmitting}>Confirmar</Button>
 			</FormActions>
 		</ConfirmForm>
